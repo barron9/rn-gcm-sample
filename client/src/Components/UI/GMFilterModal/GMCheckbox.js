7cce7d3c3e8e878fc1c51841579c4fc83acab165
berkin.tatlisu <berkin.tatlisu@NB-0201>
2019-01-23 17:27:21 +0300

    export default LAR DUZELTİLDİ

--- client/src/Components/UI/GMFilterModal/GMCheckbox.js
+++ client/src/Components/UI/GMFilterModal/GMCheckbox.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import { View, TouchableOpacity } from 'react-native';
 import FoundationIcon from 'react-native-vector-icons/Foundation';
 
-export default GMCheckbox = (props) => {
-
+const ficon = <FoundationIcon name="check" size={20} color="orange" />
+const GMCheckbox = (props) => {
 
 
     return (
         <TouchableOpacity onPress={() => props.onpress && props.onpress()} activeOpacity={1}
             style={{ flexDirection: 'row', margin: 5, alignItems: 'center', justifyContent: 'center', width: 30, height: 30, borderRadius: 5, borderColor: 'gray', borderWidth: .5 }}>
-            {!props.selected && <FoundationIcon name="check" size={20} color="orange" />}
+            {!props.selected && ficon}
         </TouchableOpacity>
     );
 
@@ -22,3 +22,5 @@ GMCheckbox.propTypes = {
     selected: PropTypes.bool,
     onpress: PropTypes.fun
 };
+
+export default GMCheckbox
--- client/src/Components/UI/GMFilterModal/GMStars.js
+++ client/src/Components/UI/GMFilterModal/GMStars.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FoundationIcon from 'react-native-vector-icons/Foundation';
-import { View,Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 
 
-let arr = [1,2,3,4,5]
+let arr = [1, 2, 3, 4, 5]
 
-export default GMStars = (props) => {
-    
+const GMStars = (props) => {
+    const ficon = arr.map((item, i) => {
         return (
-            <View style={{ flexDirection: 'row', margin: 5, alignItems: 'center' }}>
-                {arr.map( (item, i) => {
-                    return (
-                        <FoundationIcon name="star" size={20} color={i <props.stars ? "orange" : "gray"} style={{ marginRight: 3 }} />
-                    ); 
-                }, this)}
-                <Text style={{ color: '#ccc', fontSize: 14, margin: 5 }}>({props.comments})</Text>
-            </View>
-        ); 
-    
+            <FoundationIcon name="star" size={20} color={i < props.stars ? "orange" : "gray"} style={{ marginRight: 3 }} />
+        );
+    })
+    return (
+        <View style={{ flexDirection: 'row', margin: 5, alignItems: 'center' }}>
+            {ficon}
+            <Text style={{ color: '#ccc', fontSize: 14, margin: 5 }}>({props.comments})</Text>
+        </View>
+    );
+
 }
 GMStars.propTypes = {
     stars: PropTypes.number,
     comments: PropTypes.number,
-    color:PropTypes.string
+    color: PropTypes.string
 };
+
+export default GMStars
